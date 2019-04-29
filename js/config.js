e1897137ef3e7e6d11aab594e7f394caebb0fe92
Jason Kwong <jasonkwong@jason-kwongs-macbook.local>
2019-04-29 12:44:44 -0400

    update

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+    auth0: {
+        domain: 'xbox-cure.auth0.com',
+        clientId: 'W725GXd2IwCQ1k76JbETteoo5y49zwWz'
+    },
+    apiBaseUrl: 'https://ot0wntjad0.execute-api.us-east-1.amazonaws.com/dev'
+};
